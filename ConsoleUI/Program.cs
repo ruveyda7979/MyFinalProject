6de22447489b4799528388db292afd448963fb1c
Rüveyda Koçaker <rvydkocaker@gmail.com>
2024-03-31 03:07:27 +0300

    updatee

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,11 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 
 
+//SOLID
+//Open closed principle: Yaptığın yazılıma
+//yeni bir özellik ekliyorsan mevcuttaki hiçbir koduna dokunamazsın.
 
-ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+
+ProductManager productManager = new ProductManager(new EfProductDal() );
 foreach (var product in productManager.GetAll())
 {
     Console.WriteLine(product.ProductName);
